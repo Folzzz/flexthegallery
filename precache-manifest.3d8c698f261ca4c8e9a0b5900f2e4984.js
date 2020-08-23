dc924f356374bd812f2f61af2fae6861d93dda53
Olaniyan Foluso <olaniyanfoluso498@gmail.com>
2020-08-23 03:25:14 +0100

    Updates

---
+++ precache-manifest.3d8c698f261ca4c8e9a0b5900f2e4984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e5959b2204e57f8d4551e9e97c2e91",
+    "url": "/flexthegallery/index.html"
+  },
+  {
+    "revision": "a864cea5a0cec30a68bb",
+    "url": "/flexthegallery/static/css/main.a20d66d9.chunk.css"
+  },
+  {
+    "revision": "9fc2ff411f7836ed30d7",
+    "url": "/flexthegallery/static/js/2.2585a008.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flexthegallery/static/js/2.2585a008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a864cea5a0cec30a68bb",
+    "url": "/flexthegallery/static/js/main.7dffdd1b.chunk.js"
+  },
+  {
+    "revision": "05f17639cc4e4a7c2a89",
+    "url": "/flexthegallery/static/js/runtime-main.b4965541.js"
+  }
+]);
